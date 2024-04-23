7d8308a94c868e9d95b6699c34c6f9cd002a56e4
GodReorx <eduard.granero@gmail.com>
2024-04-23 18:27:53 +0200

    Security añadido al config, URLs para mirar en sinimplementar.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,19 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.2.4")
-	//implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.4")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.5")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.2.5")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.2.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	//implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
 	//implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	//testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/games/security/sinimplementar.java
+++ src/main/java/com/games/security/sinimplementar.java
@@ -1,4 +1,24 @@
 package com.games.security;
 
-public class sinimplementar {
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class sinimplementar implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
+
+
+/*
+
+https://medium.com/@truongbui95/jwt-authentication-and-authorization-with-spring-boot-3-and-spring-security-6-2f90f9337421
+
+https://www.geeksforgeeks.org/spring-boot-3-0-jwt-authentication-with-spring-security-using-mysql-database/
+
+https://medium.com/spring-boot/spring-boot-3-spring-security-6-jwt-authentication-authorization-98702d6313a5
+
+ */
